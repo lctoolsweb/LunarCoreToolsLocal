4a7671452a2ec5836595cc4372044c72143bd78b
Enforcer <45286708+niizam@users.noreply.github.com>
2023-12-20 09:36:50 +0700

    Update index.adb2d8d6.js

--- assets/index.adb2d8d6.js
+++ assets/index.adb2d8d6.js
@@ -1,1 +1,48 @@
-import{_ as r}from"./index.d12402ed.js";import{_ as a}from"./plugin-vue_export-helper.21dcd24c.js";import{r as n,c,a as t,b as e,w as _,F as i,o as d,d as l}from"./index.16ec0bdc.js";/* empty css              *//* empty css              *//* empty css               */const m={},u={class:"ct"},p=e("div",{class:"title"}," LunarCoreTools ",-1),f=e("div",{class:"introduce"}," Luncore\u63A7\u5236\u53F0\u4EE3\u7801\u5FEB\u901F\u751F\u6210 ",-1),x={class:"start"};function v(h,k){const o=r,s=n("router-link");return d(),c(i,null,[t(o),e("div",u,[p,f,e("div",x,[t(s,{to:"/start/commuse",class:"mr-3 flex-none w-[3.0625rem] md:w-auto leading-6 dark:text-slate-200"},{default:_(()=>[l(" \u5F00\u59CB ")]),_:1})])])],64)}var y=a(m,[["render",v]]);export{y as default};
+import {
+    _ as r
+} from "./index.d12402ed.js";
+import {
+    _ as a
+} from "./plugin-vue_export-helper.21dcd24c.js";
+import {
+    r as n,
+    c,
+    a as t,
+    b as e,
+    w as _,
+    F as i,
+    o as d,
+    d as l
+} from "./index.16ec0bdc.js"; /* empty css              */ /* empty css              */ /* empty css               */
+const m = {},
+    u = {
+        class: "ct"
+    },
+    p = e("div", {
+        class: "title"
+    }, " LunarCoreTools ", -1),
+    f = e("div", {
+        class: "introduce"
+    }, " Luncore Quick generation of console code ", -1),
+    x = {
+        class: "start"
+    };
+
+function v(h, k) {
+    const o = r,
+        s = n("router-link");
+    return d(), c(i, null, [t(o), e("div", u, [p, f, e("div", x, [t(s, {
+        to: "/start/commuse",
+        class: "mr-3 flex-none w-[3.0625rem] md:w-auto leading-6 dark:text-slate-200"
+    }, {
+        default: _(() => [l(" Start ")]),
+        _: 1
+    })])])], 64)
+}
+var y = a(m, [
+    ["render", v]
+]);
+export {
+    y as
+    default
+};
